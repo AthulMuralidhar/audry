8a7603b59efd24a2e56d72f7fbf45459261cbda2
Athul Muralidhar <athul.m68@gmail.com>
2019-06-27 17:56:20 +0200

    added ML code for DeepVoice

--- audry.js
+++ audry.js
@@ -1,1 +1,4 @@
+// small extension:
 //https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension
+// more complex extension:
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
